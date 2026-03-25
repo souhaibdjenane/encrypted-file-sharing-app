031c15d4fa26c25bd018c89ba8953f443ed3d594
souhaibdjenane <souhaibdjenane@gmail.com>
2026-03-25 15:31:15 +0100

    jj

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,6 +1,5 @@
 import { FileUploader } from '@/components/files/FileUploader'
 import { FileList } from '@/components/files/FileList'
-import { SharedFileList } from '@/components/files/SharedFileList'
 import { useAuthStore } from '@/store/authStore'
 
 export function DashboardPage() {
@@ -34,13 +33,6 @@ export function DashboardPage() {
         <FileList />
       </div>
 
-      {/* Shared Section */}
-      <div className="mt-12">
-        <h2 className="text-sm font-semibold text-zinc-400 uppercase tracking-wider mb-3">
-          Shared with you
-        </h2>
-        <SharedFileList />
-      </div>
     </div>
   )
 }
